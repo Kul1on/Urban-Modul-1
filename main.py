1d016d2c4473c00e25153345b63bd5b1536d3902
Kul1on <163291724+Kul1on@users.noreply.github.com>
2024-04-04 10:01:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+
+'''
+#Два предыдущих кода
+
+def print_params(a=1, b='bruh', c=True):
+
+    print(a, b, c)
+
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = [1, 'sad', False]
+values_dict = {'a': 5, 'b': 'Who', 'c': False}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [4, 'good']
+print_params(*values_list_2, 42)
 def def_test(*args):
     for arg in args:
         print(arg)
@@ -12,3 +32,12 @@ def factorial(n):
 def_test(1, 'bruh', [1, 2, 3], True)
 
 print(factorial(6))
+
+def print_helloworld(*args):
+        print("Hello world")
+
+'''
+
+print_helloworld()
+print_helloworld()
+
