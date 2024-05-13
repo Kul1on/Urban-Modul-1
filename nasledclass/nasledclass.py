c0d859546bf53fc45c41408247a9166f4fd2e632
Kul1on <163291724+Kul1on@users.noreply.github.com>
2024-05-13 13:42:55 +0300

    nasledclass.py

--- nasledclass/nasledclass.py
+++ nasledclass/nasledclass.py
@@ -2,19 +2,16 @@ class Car:
     price = 1000000
 
     def horse_powers(self):
-        return 100  
+        return 100
+
 class Nissan(Car):
-    def __init__(self):
-        super().__init__() 
-        self.price = 2000000  
+    price = 2000000
 
     def horse_powers(self):
-        return 200 
+        return 200
 
 class Kia(Car):
-    def __init__(self):
-        super().__init__()  
-        self.price = 3000000  
+    price = 3000000
 
     def horse_powers(self):
-        return 300  
+        return 150
