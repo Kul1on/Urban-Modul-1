f8f0ca3e614d9d819485a49e13bf084fec959a36
Kul1on <163291724+Kul1on@users.noreply.github.com>
2024-10-27 20:14:27 +0300

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,17 +1,14 @@
-# Родительский класс для животных
 class Animal:
     def __init__(self, name):
         self.name = name
         self.alive = True
         self.fed = False
 
-# Родительский класс для растений
 class Plant:
     def __init__(self, name):
         self.name = name
-        self.edible = False  # По умолчанию несъедобное
+        self.edible = False  
 
-# Класс млекопитающих, наследуется от Animal
 class Mammal(Animal):
     def eat(self, food):
         if food.edible:
@@ -21,7 +18,6 @@ class Mammal(Animal):
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
 
-# Класс хищников, наследуется от Animal
 class Predator(Animal):
     def eat(self, food):
         if food.edible:
@@ -31,29 +27,27 @@ class Predator(Animal):
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
 
-# Класс цветков, наследуется от Plant
 class Flower(Plant):
-    pass  # Цветы по умолчанию несъедобны, дополнительных изменений не требуется
+    pass  
 
-# Класс фруктов, наследуется от Plant
 class Fruit(Plant):
     def __init__(self, name):
         super().__init__(name)
-        self.edible = True  # Переопределяем съедобность для фруктов
+        self.edible = True  
 
-# Пример использования
+
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
 
-# Вывод для проверки
+
 print(a1.name)
 print(p1.name)
 
 print(a1.alive)  
 print(a2.fed)    
-a1.eat(p1)       # Волк с Уолл-Стрит не стал есть Цветик семицветик
-a2.eat(p2)       # Хатико съел Заводной апельсин
-print(a1.alive)  # False
-print(a2.fed)    # True
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed) 
