042deb6d9e0f09acae38e99dc17a907d3b96a1f2
Kul1on <163291724+Kul1on@users.noreply.github.com>
2024-10-28 07:15:51 +0300

    Create module_8_1.py

---
+++ module_8_1.py
@@ -0,0 +1,9 @@
+def add_everything_up(a, b):
+    if isinstance(a, str) or isinstance(b, str):
+        return str(a) + str(b)
+    else:
+        return a + b
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
